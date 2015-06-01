33a71d1f3b53de2232547d97b881ea57ee16edd3
Ivan Suftin <isuftin@usgs.gov>
2015-06-01 15:58:31 -0500

    Added retrieve trigger script

---
+++ scripts/trigger-retrieve.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+cd ${0%/*}
+
+docker_exec=$(which /usr/bin/docker)
+server_ps=$(${docker_exec} ps -a -q -f name=spotweb_server)
+
+$docker_exec exec $server_ps /usr/bin/php /var/www/spotweb/retrieve.php
