6c5635cd3356114fa2364e39aa486998e2852855
Ivan Suftin <isuftin@usgs.gov>
2015-06-02 10:42:21 -0500

    Updated to use variables instead of hardcoding

--- scripts/build-all.sh
+++ scripts/build-all.sh
@@ -2,5 +2,5 @@
 
 source base.sh
 
-$docker_exec build -t ${name_db}:latest ../${name_db}/
-$docker_exec build -t spotweb:latest ../${name_server}/
+$docker_exec build -t isuftin/${name_db}:latest ../${name_db}/
+$docker_exec build -t isuftin/spotweb:latest ../${name_server}/
