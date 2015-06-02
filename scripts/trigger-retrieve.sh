ed173591d2fc2540ff685564fe378494ce340ea6
Ivan Suftin <isuftin@usgs.gov>
2015-06-02 06:45:57 -0500

    Added a debug flag for retrieve script

--- scripts/trigger-retrieve.sh
+++ scripts/trigger-retrieve.sh
@@ -5,5 +5,5 @@ cd ${0%/*}
 docker_exec=$(which /usr/bin/docker)
 server_ps=$(${docker_exec} ps -a -q -f name=spotweb_server)
 
-$docker_exec exec $server_ps /usr/bin/php /var/www/spotweb/retrieve.php | tee /var/log/spotweb-retrieve.log
+$docker_exec exec $server_ps /usr/bin/php /var/www/spotweb/retrieve.php --debug | tee /var/log/spotweb-retrieve.log
 
