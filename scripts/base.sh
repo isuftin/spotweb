ab3943341b84ca5327d601c9540ed5f2d6378c62
Ivan Suftin <isuftin@usgs.gov>
2015-06-06 17:29:50 -0500

    Fix docker exec locator

--- scripts/base.sh
+++ scripts/base.sh
@@ -6,7 +6,7 @@ cd ${0%/*}
 name_db=spotweb_db
 name_server=spotweb_server
 name_data=spotweb_data
-docker_exec=$(which /usr/bin/docker)
+docker_exec=$(which docker)
 db_ps=$($docker_exec ps -a -q -f name=$name_db)
 server_ps=$($docker_exec ps -a -q -f name=$name_server)
 data_ps=$($docker_exec ps -a -q -f name=$name_data)
